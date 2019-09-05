fcf2688e6fea32e65014ed5e82749b162b7bc9c0
Angelos Charalambidis <agcharal@gmail.com>
2019-09-05 14:39:27 +0300

    Change the criterion for grad identification

--- tensorflow/core/grappler/optimizers/function_transformation.cc
+++ tensorflow/core/grappler/optimizers/function_transformation.cc
@@ -285,7 +285,7 @@ Status CallRewriter::CollectCalls(std::vector<CallInfo>& calls) {
             return errors::InvalidArgument("Cannot find forward node for gradient ",
                     ngrad->name());
         }
-        CallInfo* fwd_call = *fwd_call_it;
+        CallInfo* fwd_call = fwd_call_it->second;
         fwd_call->grad_node = ngrad;
     }
 
