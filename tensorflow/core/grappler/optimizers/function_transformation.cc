fd858c873de57e7922617968b5d061bcdb0baf2e
Angelos Charalambidis <agcharal@gmail.com>
2019-09-12 15:05:10 +0300

    Print func def

--- tensorflow/core/grappler/optimizers/function_transformation.cc
+++ tensorflow/core/grappler/optimizers/function_transformation.cc
@@ -538,6 +538,45 @@ Status InlineFunction(const FunctionDef& func_def,
     return Status::OK();
 }
 
+
+string Print(const FunctionDef& fdef) {
+  string out;
+  const OpDef& sig = fdef.signature();
+  strings::StrAppend(&out, "\n", sig.name());
+  if (sig.attr_size() > 0) {
+    strings::StrAppend(&out, "[");
+    for (int i = 0; i < sig.attr_size(); ++i) {
+      const auto& a = sig.attr(i);
+      if (i > 0) strings::StrAppend(&out, ", ");
+      if (a.type() == "type") {
+        strings::StrAppend(&out, a.name(), ":", Print(a.allowed_values()));
+      } else {
+        strings::StrAppend(&out, a.name(), ":", a.type());
+      }
+    }
+    strings::StrAppend(&out, "]");
+  }
+  strings::StrAppend(&out, "(");
+  for (int i = 0; i < sig.input_arg_size(); ++i) {
+    if (i > 0) strings::StrAppend(&out, ", ");
+    strings::StrAppend(&out, Print(sig.input_arg(i)));
+  }
+  strings::StrAppend(&out, ") -> (");
+  for (int i = 0; i < sig.output_arg_size(); ++i) {
+    if (i > 0) strings::StrAppend(&out, ", ");
+    strings::StrAppend(&out, Print(sig.output_arg(i)));
+  }
+  strings::StrAppend(&out, ") {\n");
+  for (const auto& n : fdef.node_def()) {
+    strings::StrAppend(&out, "  ", Print(n), "\n");
+  }
+  for (const auto& r : fdef.ret()) {
+    strings::StrAppend(&out, "  return ", r.first, " = ", r.second, "\n");
+  }
+  strings::StrAppend(&out, "}\n");
+  return out;
+}
+
 Status InlineFunctionAndGradient(const FunctionDef& func_def,
                       const FunctionInliningContext& ctx,
                       const std::unordered_map<string, AttrValue>& func_attr,
@@ -552,6 +591,8 @@ Status InlineFunctionAndGradient(const FunctionDef& func_def,
                 "Invalid argument, function ", grad_name, "can not be found",
                 "or not marked to be inlined");
     }
+
+    Print(*grad_def);
 
     /************* Print the Gradient graph of func def ******************/
     std::unique_ptr<GrapplerItem> temp_item = GrapplerItemFromFunctionDef(*grad_def, func_attr, ctx.Library());
