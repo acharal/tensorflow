425a462be438147bc90200d73325eb20b1b03f9b
Calliope Kostopoulou <kelkost@yahoo.gr>
2019-09-17 18:55:13 +0300

    mnist exp

---
+++ TESTS/experiments/mnist_func.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.framework import function
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# Python optimisation variables
+learning_rate = 0.5
+epochs = 1
+batch_size = 10
+
+# # now declare the weights connecting the input to the hidden layer
+# W1 = tf.get_variable('W1', [784, 300], initializer=tf.constant_initializer(0.0))
+# b1 = tf.get_variable('b1', [300], initializer=tf.constant_initializer(0.0))
+
+# # and the weights connecting the hidden layer to the output layer
+# W2 = tf.get_variable('W2', [300, 10], initializer=tf.constant_initializer(0.0))
+# b2 = tf.get_variable('b2', [10], initializer=tf.constant_initializer(0.0))
+
+W1 = tf.get_variable('W2', [784, 10], initializer=tf.constant_initializer(0.0))
+b1 = tf.get_variable('b2', [10], initializer=tf.constant_initializer(0.0))
+
+func = function.Declare("Func", [("x", tf.float32), ("W1", tf.float32), ("b1", tf.float32)], [("ret", tf.float32)])
+@function.Defun(tf.float32, tf.float32, tf.float32, func_name="Func", create_grad_func=True, out_names=["ret"])
+def FuncImpl(x, W1, b1):
+	hidden_out = tf.nn.relu(tf.add(tf.matmul(x, W1), b1))
+	# y_est = tf.nn.softmax(tf.add(tf.matmul(hidden_out, W2), b2))
+	return hidden_out
+
+FuncImpl.add_to_graph(tf.get_default_graph())
+
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+y_est = func(x, W1, b1)
+y_clipped = tf.clip_by_value(y_est, 1e-10, 0.9999999)
+cross_entropy = tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped) + (1 - y) * tf.log(1 - y_clipped), axis=1))
+optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_est, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+
+file = open("mnist_output.txt", "w") 
+
+# start the session
+with tf.Session() as sess:
+	# initialise the variables
+	sess.run(tf.initialize_all_variables())
+	total_batch = int(len(mnist.train.labels) / batch_size)
+	for epoch in range(epochs):
+		avg_cost = 0
+	
+		for i in range(total_batch):
+			batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
+			_, c = sess.run([optimiser, cross_entropy], feed_dict={x: batch_x, y: batch_y})
+			avg_cost += c / total_batch
+	
+			exit(1)
+	file.write("Epoch:" + str(epoch+1) + " cost =" + str(avg_cost) + "\n")
+	file.write(str((sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))) + "\n")
+
+file.close()
